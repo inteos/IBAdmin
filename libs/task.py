9581dbccaec9a8bb4803d3de9b0d1c93d8281939
Radosław Korzeniewski <radekk@inteos.pl>
2018-08-11 14:40:56 +0200

    Fix tasks variable references.

--- libs/task.py
+++ libs/task.py
@@ -3,29 +3,6 @@ from __future__ import unicode_literals
 from tasks.models import *
 import os
 
-TASKSSTATUS = (
-        ('N', 'New task'),
-        ('R', 'Running task'),
-        ('F', 'Finished succesfully task'),
-        ('E', 'Finished with errors task'),
-    )
-
-TASKSPROCS = (
-        (1, 'Delete jobids for Job'),
-        (2, 'Delete jobs and jobids for Client'),
-        (3, 'Detect drives for Library'),
-        (4, 'Labeling tape volumes'),
-        (5, 'Rescanning drives for Library'),
-    )
-
-TASKSPROCSDESCR = {
-        1: 'Deleting Job from configuration with job history',
-        2: 'Deleting Client from configuration with job configuration and history',
-        3: 'Detecting tape library and drives',
-        4: 'Initializing tape library volumes for Bacula',
-        5: 'Rescanning tape library for hardware changes',
-    }
-
 
 def getTasksInfo():
     tasks = Tasks.objects.filter(status__in=['R', 'N', 'E']).order_by('-taskid').all().values()
--- tasks/models.py
+++ tasks/models.py
@@ -1,7 +1,30 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from libs.task import TASKSSTATUS, TASKSPROCS
+
+
+TASKSSTATUS = (
+        ('N', 'New task'),
+        ('R', 'Running task'),
+        ('F', 'Finished succesfully task'),
+        ('E', 'Finished with errors task'),
+    )
+
+TASKSPROCS = (
+        (1, 'Delete jobids for Job'),
+        (2, 'Delete jobs and jobids for Client'),
+        (3, 'Detect drives for Library'),
+        (4, 'Labeling tape volumes'),
+        (5, 'Rescanning drives for Library'),
+    )
+
+TASKSPROCSDESCR = {
+        1: 'Deleting Job from configuration with job history',
+        2: 'Deleting Client from configuration with job configuration and history',
+        3: 'Detecting tape library and drives',
+        4: 'Initializing tape library volumes for Bacula',
+        5: 'Rescanning tape library for hardware changes',
+    }
 
 
 # Create your models here.
