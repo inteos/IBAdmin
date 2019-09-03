583f6ac46ac2b2be49744c67c9427432049ce54f
Radosław Korzeniewski <radekk@inteos.pl>
2019-09-03 19:29:44 +0200

    Update copyright on departments app.

--- departments/forms.py
+++ departments/forms.py
@@ -1,3 +1,9 @@
+# -*- coding: UTF-8 -*-
+#
+#  Copyright (c) 2015-2019 by Inteos Sp. z o.o.
+#  All rights reserved. See LICENSE file for details.
+#
+
 from __future__ import unicode_literals
 from libs.widgets import *
 from libs.forms import LABELCOLORS
